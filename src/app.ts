a3c6200e94eb297f1fbb0f95e2474c90d94a46ad
vitalragaz <kkrause95@gmail.com>
2023-06-14 22:36:31 +0200

    fix: :bug: Added validators

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,19 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   const { url, quality = "highest" } = req.query;
+
+  if (!url) {
+    return res.status(400).json({
+      message: "URL is required",
+    });
+  }
+
+  if (!ytdl.validateURL(url as string)) {
+    return res.status(400).json({
+      message: "URL is invalid",
+    });
+  }
+
   ytdl(url as string, {
     quality: quality as string,
   }).pipe(res);
